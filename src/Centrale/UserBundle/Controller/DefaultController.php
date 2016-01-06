a93cb0591b4e45598e6b44e09817403453655991
fabien.rondeau@stadline.com <fabien.rondeau@stadline.com>
2016-01-06 10:25:58 +0100

    Update Test and Controller

--- src/Centrale/UserBundle/Controller/DefaultController.php
+++ src/Centrale/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Util\Debug;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
@@ -17,7 +18,6 @@ class DefaultController extends Controller
      */
     public function indexAction($firstname, $lastname)
     {
-//        dump($this->container, $this->getRequest());
 
         return $this->render('CentraleUserBundle:Default:index.html.twig', array(
             'firstname' => $firstname,
@@ -29,15 +29,43 @@ class DefaultController extends Controller
      * @Route("/wall/{firstname}/{lastname}", name="centrale_user_bundle_wall")
      * @Template("")
      */
-    public function wallAction($firstname, $lastname)
+    public function wallAction(Request $request, $firstname, $lastname)
     {
+
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository('CentraleUserBundle:Post')->findAll();
+
+        $newPost = new Post();
+        $newPost->setCreatedAt(new \DateTime());
+        $newPost->setAuthor($firstname.' '.$lastname);
+
+        $form = $this->createFormBuilder($newPost)
+                ->setAction($this->generateUrl("centrale_user_bundle_wall", array(
+                            'firstname' => $firstname,
+                            "lastname" => $lastname)
+                ))
+                ->setMethod('POST')
+                ->add('created_at', 'datetime', array("label" => "Créé le", "widget" => 'single_text'))
+                ->add('message', 'textarea')
+                ->add('author')
+                ->add('save', 'submit')
+                ->getForm();
+
+        if($request->getMethod() == "POST") {
+            $form->handleRequest($request);
+            if($form->isValid()){
+                $post = $form->getData();
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($post);
+                $em->flush();
+            }
+        }
 
         return array(
             'posts' => $posts,
             'firstname' => $firstname,
-            "lastname" => $lastname
+            "lastname" => $lastname,
+            "form" => $form->createView()
         );
     }
 }
--- src/Centrale/UserBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Centrale/UserBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testSecureArea()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/wall/fabien/rondeau');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $client->followRedirect();
+        $this->assertRegExp("/Connexion/", $client->getResponse()->getContent());
+    }
+
+    public function testWallAction()
+    {
+        $client = static::createClient();
+
+        // login into action
+        $crawler = $client->request('GET', '/login');
+        $this->assertRegExp("/Connexion/", $client->getResponse()->getContent());
+
+        // get the form
+        $form = $crawler->selectButton('_submit')->form(array(
+            '_username' => 'fabien',
+            '_password' => 'fabien',
+        ));
+
+        // submit the form to the client
+        $crawler = $client->submit($form);
+
+        // follow Login redirect
+        $client->followRedirect();
+
+        // follow homepage redirect
+        $client->followRedirect();
+
+        // check we are on the right page
+        $this->assertRegExp("/fabien rondeau/", $client->getResponse()->getContent());
+
     }
 }
